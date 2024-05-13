34d86b10c4e93d7bb0424cc379bf56daaf74d462
sham savvasher <yoyoshamsavvasher@gmail.com>
2024-05-13 14:28:30 +0530

    Namaste React 5th Episode

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,30 @@
 import RestaurantCard from "./RestaurantCard";
 import { resList } from "../utils/mockData";
+import { useState } from "react";
+
+
 const Body = () => {
+    const [listOfRestaurants, setlistOfRestaurants] = useState(resList);
+ 
     return (
         <div className="body">
 
 
             <div className="filter">
                 <button className="filter-btn"
-                onClick={() => { console.log("button clicked") }} onMouseOver={()=>{console.log("hover")}}>Top Rated Restaurant</button>
+                    onClick={() => { 
+                        
+                        const filterdList = listOfRestaurants.filter(
+                            (res) => res.info.avgRating > 4.5
+                        );
+                        setlistOfRestaurants(filterdList)
+                }}>Top Rated Restaurant</button>
             </div>
 
 
             <div className="res-container">
                 {
-                    resList.map((Restaurant) => (
+                    listOfRestaurants.map((Restaurant) => (
                         <RestaurantCard key={Restaurant.info.id } resData={Restaurant}/>
                     ))}
                 
